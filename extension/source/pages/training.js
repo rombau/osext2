3c3100773646af1708ec47593e6afd870f477fda
rombau <os@greenmoon.at>
2022-02-02 16:35:01 +0100

    training chance considered when extracting

--- extension/source/pages/training.js
+++ extension/source/pages/training.js
@@ -27,9 +27,8 @@ Page.Training = class extends Page {
 			else if (skill === 'SPL') skill = 'DEC';
 			else if (skill === 'REF') skill = 'GES';
 
-			// TODO Chance = "Trainerskill zu niedrig!" 
-
-			if (!injured && trainerNr > 0 && Object.keys(Skill).includes(skill)) {
+			if (!injured && trainerNr > 0 && Object.keys(Skill).includes(skill) && 
+				row.cells['Chance'].textContent.search(' %') != -1) {
 			
 				let id = HtmlUtil.extractIdFromHref(row.cells['Name'].firstChild.href);
 				let player = data.team.getSquadPlayer(id); 
