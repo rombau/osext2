f514c4e41bf0e87fc70c6bc11772514b60c1dae3
rombau <os@greenmoon.at>
2023-12-06 19:09:13 +0100

    handle browser_specific_settings in manifest

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,15 @@ const zip = require('gulp-zip');
 const del = require('del');
 const argv = require('minimist')(process.argv.slice(2));
 const release = require('gulp-github-release');
+const jsonTransform = require('gulp-json-transform');
 
 gulp.task('clean', () => {
 	return del('./release/*.zip');
 });
 
-gulp.task('zip', () => {
+gulp.task('zip-firefox', () => {
 	return gulp.src('./extension/**')
-		.pipe(zip('osext_' + argv.release + '.zip'))
+		.pipe(zip('osext_' + argv.release + '_firefox.zip'))
 		.pipe(gulp.dest('./release'));
 });
 
@@ -37,7 +38,7 @@ gulp.task('git-push', (cb) => {
 });
 
 gulp.task('release', () => {
-	return gulp.src('./release/osext_' + argv.release + '.zip')
+	return gulp.src('./release/osext_' + argv.release + '*.zip')
 		.pipe(release({
 			owner: 'rombau',
 			repo: 'osext2',
@@ -49,4 +50,30 @@ gulp.task('release', () => {
 		}));
 });
 
-gulp.task('default', gulp.series('clean', 'bump', 'git-add', 'git-commit', 'git-push', 'zip', 'release'));
+gulp.task('prepare-chrome', () => {
+	return gulp.src('./extension/**')
+		.pipe(gulp.dest('./chrome'));
+});
+
+gulp.task('clean-chrome-manifest', () => {
+	return gulp.src('./chrome/manifest.json')
+		.pipe(jsonTransform((json, file) => {
+			if (file.relative == 'manifest.json') {
+				delete json['browser_specific_settings'];
+			}
+			return json;
+		}, "\t"))
+		.pipe(gulp.dest('./chrome'));
+});
+
+gulp.task('zip-chrome', () => {
+	return gulp.src('./chrome/**')
+		.pipe(zip('osext_' + argv.release + '_chrome.zip'))
+		.pipe(gulp.dest('./release'));
+});
+
+gulp.task('del-chrome', () => {
+	return del('./chrome');
+});
+
+gulp.task('default', gulp.series('clean', 'bump', 'git-add', 'git-commit', 'git-push', 'zip-firefox', 'release', 'prepare-chrome', 'clean-chrome-manifest', 'zip-chrome', 'del-chrome'));
