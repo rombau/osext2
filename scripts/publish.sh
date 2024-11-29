dfd30e39b8098f10066cd5cc28d9581505a00e8e
rombau <os@greenmoon.at>
2024-11-29 20:20:29 +0100

    fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -36,7 +36,8 @@ do
     sleep 1;
 done
 curl -L -s -H "Authorization: $JWT" \
-	-d '{ "upload": "'"$UPLOAD_ID"'" }' \
+	-H "Content-Type: application/json" \
+	-d '{"upload": "'"$UPLOAD_ID"'"}' \
 	"https://addons.mozilla.org/api/v5/addons/addon/$EXTENSION_ID/versions/"
 echo " OK"
 
@@ -47,15 +48,15 @@ echo -n "Publish extension on Google Web Store ..."
 EXTENSION_ID=plhbikhfgkkommnghjaihagamgophofm
 
 GWS_TOKEN=$(curl -L -s \
-	-d "client_secret=$GWS_API_SECRET&grant_type=refresh_token&refresh_token=1$GWS_API_REFRESHTOKEN&client_id=$GWS_API_KEY"
+	-d "client_id=$GWS_API_KEY&client_secret=$GWS_API_SECRET&grant_type=refresh_token&refresh_token=$GWS_API_REFRESHTOKEN" \
 	"https://oauth2.googleapis.com/token" | jq -r '.access_token')
 echo -n "."
-UPLOAD_RESPONSE=$(curl -L -s -X PUT 
+UPLOAD_RESPONSE=$(curl -L -s \
 	-H "Authorization: Bearer $GWS_TOKEN" \
 	-T $(find release -name 'osext*chrome.zip') \
 	"https://www.googleapis.com/upload/chromewebstore/v1.1/items/$EXTENSION_ID")
 echo -n "."
-UPLOAD_RESPONSE=$(curl -L -s -X POST
+UPLOAD_RESPONSE=$(curl -L -s -X POST \
 	-H "Authorization: Bearer $GWS_TOKEN" \
 	"https://www.googleapis.com/chromewebstore/v1.1/items/$EXTENSION_ID/publish")
 echo " OK"
